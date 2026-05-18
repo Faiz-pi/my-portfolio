9985d74851aff151a289129ed3bd187dc4ad6c44
Faiz <mohdfaiz182005@gmail.com>
2026-05-18 14:43:12 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let resume = document.getElementById("resume");
+resume.addEventListener("click", () => {
+  window.location.href = "src/MY RESUME.pdf";
+});
+
+let instagram = document.getElementById("instagram");
+instagram.addEventListener("click", () => {
+  window.location.href =
+    "https://www.instagram.com/captain_faiz_01?igsh=cmlmOXB6OGZrNmM2";
+});
+let mail = document.getElementById("mail");
+mail.addEventListener("click", () => {
+  window.location.href =
+    "mailto:mohdfaiz182005@gmail.com?subject=Let's%20Connect&body=Hi%20Faiz,%20I%20saw%20your%20portfolio%20and%20wanted%20to%20reach%20out!";
+});
+
+let x = document.getElementById("X");
+x.addEventListener("click", () => {
+  window.location.href = "https://x.com/MohdFai67290259?s=09";
+});
